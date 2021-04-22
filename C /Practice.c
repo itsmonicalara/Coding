7ce79fdc3d2179db9c0b83e3c52b9c4232678a24
Monica Lara <A01655306@itesm.mx>
2021-04-21 20:43:04 -0500

    Erased conflict files

--- C /Practice.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main(int argc, char * argv[]){
-	printf("Hola %s, el número de argumentos que pasaste fue de %d\n", argv[0], argc);
-	short s = 32766;
-	s++;
-	printf("El valor %i\n", s);
-	return 0;
-}
