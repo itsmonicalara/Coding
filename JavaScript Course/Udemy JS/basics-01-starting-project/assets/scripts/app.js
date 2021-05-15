f4a418124cea6ec187e88e95142aead27e855d01
Monica Lara <38739285+itsmonicalara@users.noreply.github.com>
2021-05-14 23:04:48 -0500

    Todays work
    2nd assigment missing

--- JavaScript Course/Udemy JS/basics-01-starting-project/assets/scripts/app.js
+++ JavaScript Course/Udemy JS/basics-01-starting-project/assets/scripts/app.js
@@ -2,6 +2,11 @@
 //This value will never change
 const defaultResult = 0;
 let currentResult = defaultResult;
-currentResult = currentResult + 10 * 3;
-let calculationDescription = '(' +currentResult+ ')'
-outputResult(currentResult, calculationDescription);
+
+function add() {
+  currentResult = currentResult + userInput.value;
+  outputResult(currentResult, '');
+}
+
+addBtn.addEventListener('click', add);
+
