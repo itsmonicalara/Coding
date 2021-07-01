2738e94fc721982fcb07755c8576be8bc2fb96b7
Monica Lara <38739285+itsmonicalara@users.noreply.github.com>
2021-06-30 20:37:23 -0600

    Binary to Integers
    Hacker Rank - Day 10

--- Hackerank/30 Days of Coding/10Binary.py
+++ Hackerank/30 Days of Coding/10Binary.py
@@ -1,1 +1,30 @@
- 
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+def num_to_binary(num):
+    binary_list = []
+    current = 0
+    longest = 0
+    while int(num) > 0:
+        remainder = num % 2
+        num = num / 2
+        binary_list.insert(0, int(remainder))
+
+    for ele in binary_list:
+        if ele == 1:
+            current += 1
+        else:
+            longest = max(current, longest)
+            current = 0
+    print(max(current, longest))
+
+
+if __name__ == '__main__':
+    n = int(input().strip())
+    num_to_binary(n)
