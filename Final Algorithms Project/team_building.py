bcf4db932d32c01be2262d02a1ef69a1de1d91a9
Monica Lara <38739285+itsmonicalara@users.noreply.github.com>
2021-07-13 17:47:08 -0600

    Temporary

--- Final Algorithms Project/team_building.py
+++ Final Algorithms Project/team_building.py
@@ -1,7 +1,23 @@
-def main():
-    # TODO: add functionality
-    print("Hello World!")
+def team_building(my_list, n):
+    temp_list = []
+    for lst in my_list:
+        for elem in lst:
+            if elem == 0:
+                index = lst.index(0)
+                print("Not Worked")
+            elif elem == 1:
+                print("Worked")
+            # TODO: Add case if all are 1
+            # TODO: It needs to take in account all zeros
+
+        new_value = [my_list.index(lst) + 1, index + 1]
+        temp_list.append(new_value)
+    print(temp_list)
 
 
 if __name__ == '__main__':
-    main()
+    lst = []
+    n = int(input())
+    for i in range(n):
+        lst.append(list(map(int, input().rstrip().split())))
+    team_building(lst, n)
