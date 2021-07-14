9032d54294a6930cc00b412bbb74d8cee4786f2f
Monica Lara <38739285+itsmonicalara@users.noreply.github.com>
2021-07-13 18:52:15 -0600

    Fix for other cases

--- Final Algorithms Project/team_building.py
+++ Final Algorithms Project/team_building.py
@@ -1,18 +1,39 @@
-def team_building(my_list, n):
+def team_building(my_list):
     temp_list = []
+    first_team = []
+    second_team = []
     for lst in my_list:
         for elem in lst:
             if elem == 0:
                 index = lst.index(0)
                 print("Not Worked")
-            elif elem == 1:
-                print("Worked")
-            # TODO: Add case if all are 1
-            # TODO: It needs to take in account all zeros
-
         new_value = [my_list.index(lst) + 1, index + 1]
         temp_list.append(new_value)
-    print(temp_list)
+    for team in temp_list:
+        if team not in first_team and list(reversed(team)) not in first_team:
+            first_team.append(team)
+    for x in first_team:
+        print(" ".join(map(str, x)))
+
+# TODO: Add case if all are 1
+# TODO: It needs to take in account all zeros
+
+'''
+def team_building(my_list):
+    team_1 = []
+    team_2 = []
+    my_list = sorted(my_list)
+    for lst in my_list:
+        for elem in lst:
+            if elem == 0:
+                team_1.append(elem + 1)
+            else:
+                team_2.append(elem + 1)
+    if len(team_1) > len(team_2):
+        print(-1)
+    else:
+        print(team_1, team_2)
+'''
 
 
 if __name__ == '__main__':
@@ -20,4 +41,4 @@ if __name__ == '__main__':
     n = int(input())
     for i in range(n):
         lst.append(list(map(int, input().rstrip().split())))
-    team_building(lst, n)
+    team_building(lst)
