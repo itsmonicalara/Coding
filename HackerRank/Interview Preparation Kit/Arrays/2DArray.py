bae119322b1fb788b49d663c32895e44fb359c96
Monica Lara <monicalara.pineda@gmail.com>
2023-05-18 23:44:48 -0600

    Starting arrays

---
+++ HackerRank/Interview Preparation Kit/Arrays/2DArray.py
@@ -0,0 +1,6 @@
+def hourglassSum(arr):
+    return
+
+if __name__ == '__main__':
+    arr = [[1,1,1,0,0,0],[0,1,0,0,0,0],[1,1,1,0,0,0],[0,0,2,4,4,0],[0,0,0,2,0,0],[0,0,1,2,4,0]]
+    print(hourglassSum(arr))
